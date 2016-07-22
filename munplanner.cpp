6abe876cc4642d4c25f43e081a39b74898ba6d2f
Thatcher Chamberlin <j.thatcher.c@gmail.com>
2016-07-21 23:19:21 -0400

    Split program into multiple files

--- munplanner.cpp
+++ munplanner.cpp
@@ -8,18 +8,12 @@
 #include <vmmlib/vector.hpp>
 #include <vmmlib/math.hpp>
 
+#include <defs.cpp>
+#include <basicfunctions.cpp>
+
 // g++ munplanner.cpp -I ./ -o munplanner -std=c++0x
 
 using namespace std;
-
-/*
-Things needed:
-[x] Mun's position as function of time
-[x] vector math
-[x] time of flight orbit solver
-[x] position and velocity of orbit at a time - hodograph, vis viva?
-[ ] six parameters
-*/
 
 double desired_rm;      //mun periapsis
 double desired_ta = 0;      //time of arrival
@@ -28,13 +22,7 @@ double desired_ir = 0;
 double desired_rl;      //Kerbin periapsis outbound
 double desired_rr;      //Kerbin periapsis inbound
 
-const double kerbinRadius = 600000; //meters
-const double munRadius =    200000;
-const double munAltitude = 12000000;
-const double muKerbin = 3.5316e12; //m^3 s^-2
-const double muMun = 6.5138398e10;
-const double munSOI = 2429559.1;  //meters
-const vmml::vector<3,double> i (0,0,1);           //inclination vector - just z for 0 inclination
+
 
 void setParamters(){
   int toa[5];
@@ -67,94 +55,6 @@ void setParamters(){
   printf("\n----------------------------\n");
 }
 
-double parabolicTime(double rl, double rt){     //return time of flight for parabolic trajectory
-  return 1.0/3.0 * sqrt(2*(rt-rl)/muKerbin) * (rt+2*rl);
-}
-
-double semimajor(double rl, double rt, double theta){     //returns a, the semimajor axis
-  double num = rl*(rl-rt*cos(theta));
-  double denom = 2*rl-rt*(1+cos(theta));
-  return num/denom;
-}
-
-double tfl(double rl, double rt, double theta){
-  const double a = semimajor(rl,rt,theta);
-  const double c = sqrt(rl*rl+rt*rt-2*rl*rt*cos(theta));
-  const double s = .5*(rl+rt+c);
-  const double alpha = 2*asin(sqrt(s/(2*a)));
-  const double beta = 2*asin(sqrt( (s-c)/(2*a) ));
-
-  return sqrt(a*a*a/muKerbin) * ( (alpha-sin(alpha)) - (beta-sin(beta)) );
-}
-
-double delS(double rl, double rt, double theta){
-  const double c = sqrt(rl*rl+rt*rt-2*rl*rt*cos(theta));
-  return rl*rt*sin(theta)/(2*c);
-}
-
-double delAOverA(double rl, double rt, double theta){
-  double a = semimajor(rl, rt, theta);
-  double c2 = rl*rl+rt*rt-2*rl*rt*cos(theta);
-
-  double num = -2*rt*(a-rl)*sin(theta);
-  double denom = rl*rl+c2-rt*rt;
-
-  return num/denom;
-}
-
-
-double delTfl(double rl, double rt, double theta){
-  const double a = semimajor(rl,rt,theta);
-  const double c = sqrt(rl*rl+rt*rt-2*rl*rt*cos(theta));
-  const double s = .5*(rl+rt+c);
-  const double alpha = 2*asin(sqrt(s/(2*a)));
-  const double beta = 2*asin(sqrt( (s-c)/(2*a) ));
-
-  double dtfl = 3*tfl(rl,rt,theta)*delAOverA(rl,rt,theta)/2;
-  dtfl += sqrt(a/muKerbin)*( tan(alpha/2)*(delS(rl,rt,theta) - s*delAOverA(rl,rt,theta)) +
-                             tan(beta/2) *(delS(rl,rt,theta) + (s-c)*delAOverA(rl,rt,theta)));
-  return dtfl;
-}
-
-double munAngle(double time){
-  return 1.7 + (542.5/munAltitude)*time;
-}
-
-vmml::vector < 3, double > munPosition(double time){
-  double angle = munAngle(time);
-  vmml::vector< 3, double > p( munAltitude*cos(angle), munAltitude*sin(angle), 0 );
-  return p;
-}
-
-vmml::vector <3, double> munVelocity(double time){
-  double angle = munAngle(time);
-  vmml::vector<3, double> p(-542.5*sin(angle), 542.5*cos(angle), 0 );
-  return p;
-}
-
-struct MunIntercept{
-  vmml::vector<3,double> Rt;
-  vmml::vector<3,double> Rtm;
-  vmml::vector<3,double> Vt;
-  vmml::vector<3,double> Vtm;
-  double tfl;
-  double toa;
-  double theta;
-};
-
-
-//2D Orbit type
-struct Orbit{
-  double a;       //semimajor axis
-  double e;       //eccentricity
-
-  double aop;     //angle of periapsis
-  double time;    //time of periapsis
-
-  double p;       //parameter
-
-  MunIntercept intercept;
-};
 
 Orbit getOrbit(double rl, vmml::vector<3,double> Rt, double theta){
   Orbit out;
@@ -185,6 +85,7 @@ Orbit getOrbit(double _rl, MunIntercept intercept){
 }
 
 
+//This function is very likely known to work
 MunIntercept getIntercept(double rl, vmml::vector<3,double> Rt, double toa, double tf){
   vmml::vector<3,double> Rtm;
   vmml::vector<3,double> Vt;
@@ -239,13 +140,13 @@ Orbit findOrbit(double periapsis, double toa, double tof){
   //printf("Ra threshold: %f\n",RAguessingThreshold);
 
   double phi;
-  for(phi = 0; phi < 1.7; phi += .1){
+  for(phi = 0; phi < 3.14; phi += .2){
     vmml::vector<3,double> RtGuess (-munSOI*sin(munAngle(toa)+phi), munSOI*cos(munAngle(toa)+phi), 0 );
     RtGuess = RtGuess + munPosition(toa);
     I1 = getIntercept(periapsis,RtGuess,toa,tof);
 
     Ra = I1.Rtm - I1.Rtm.dot(I1.Vtm)/I1.Vtm.dot(I1.Vtm) * I1.Vtm;
-    //printf("\t-R_a: %f\n", Ra.length());
+    printf("Phi: %f  R_a: %f\n", phi, Ra.length());
     if(Ra.length()<RAguessingThreshold){
       break;
     }
